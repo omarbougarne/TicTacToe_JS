7c8ae25634dec9bc455ccf45d9b700381b870817
omarbougarne <omarbougarne98@gmail.com>
2024-08-30 14:44:19 +0100

    i can print x's and o's in my grid added feature

--- script.js
+++ script.js
@@ -1,16 +1,26 @@
 document.addEventListener('DOMContentLoaded', function(){
     let container = document.getElementById('gameContainer');
-    
+    let current = "X";
     for(let i = 0 ; i < 400 ; i++){
     let item = document.createElement('div');
     item.classList.add('item');
     item.setAttribute("id",i);
     container.appendChild(item);
-
+        //might change later i mean same loop
     item.addEventListener('click', function() {
         // console.log('Grid item ' + i + ' clicked');
         // (item.textContent =="x")?;
-        let checkEmpty = (item.textContent =="")?"X":""
+        // let checkEmpty = (item.textContent =="")?"X":""
+        // let content = ; // idk later
+        if(current == "X" && item.textContent ==""){
+            item.textContent ="X";
+            current = "O";
+        } else if(current == "O" && item.textContent ==""){
+            item.textContent ="O";
+            current = "X";
+        }
+        // if(content == "" )
+        console.log(content); //fix please check only ones per grid-area
         item.append(checkEmpty);
         // (item.textContent =="x")?;
     });
