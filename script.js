0a291bb3c2ec8c0bdb019d9fab5f51a7414cd196
omarbougarne <omarbougarne98@gmail.com>
2024-08-30 12:05:14 +0100

    added-some-logic

--- script.js
+++ script.js
@@ -1,8 +1,31 @@
 document.addEventListener('DOMContentLoaded', function(){
     let container = document.getElementById('gameContainer');
+    
     for(let i = 0 ; i < 400 ; i++){
     let item = document.createElement('div');
     item.classList.add('item');
+    item.setAttribute("id",i);
     container.appendChild(item);
+
+    item.addEventListener('click', function() {
+        // console.log('Grid item ' + i + ' clicked');
+        // (item.textContent =="x")?;
+        let checkEmpty = (item.textContent =="")?"X":""
+        item.append(checkEmpty);
+        // (item.textContent =="x")?;
+    });
     }
 });
+
+// let items = document.querySelectorAll('item');
+// for(let i = 0; i<items.length; i++){
+//     items[i].addEventListener('click', function(){
+//         items.appendChild('p');
+//         console.log('weowo');
+//     });
+// }
+
+//let p = document.createElement('p'); 
+        // p.textContent = 'New Paragraph'; 
+        // items[i].appendChild(p);
+        // console.log('fuck');
